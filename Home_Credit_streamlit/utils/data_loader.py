f43bb39fbc8d150d0d2389940a6a20f9952c95b1
janipooja3003 <janipooja3003@gmail.com>
2026-08-26 01:48:05 +0530

    Delete Home_Credit_streamlit/utils directory

--- Home_Credit_streamlit/utils/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def load_data(csv_path: str) -> pd.DataFrame:
-    df = pd.read_csv(csv_path)
-    # Standardize column names
-    df.columns = df.columns.str.strip().str.replace(" ", "_").str.replace("(", "").str.replace(")", "")
-    return df
